430d112248cb80d59afa55f2a1fa3f4ef7cc8650
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-06-30 11:28:10 +0900

    feat(TheToolbar.vue): install logo

--- src/components/molecules/VParallaxIcon.vue
+++ src/components/molecules/VParallaxIcon.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-parallax :src="src" height="300">
+  <v-parallax :src="src" height="350">
     <v-layout column align-center justify-center class="white--text">
       <h1 class="white--text mb-2 display-1 text-xs-center">{{ title }}</h1>
       <div v-if="subTitle" class="subheading mb-3 text-xs-center">
         {{ subTitle }}
       </div>
-      <v-btn class="red mt-5" dark large :to="link">{{ btnText }}</v-btn>
+      <v-flex xs1>
+        <v-img
+          :src="require('~/assets/logo/my-portfolio.png')"
+          width="100"
+          height="100"
+        ></v-img>
+      </v-flex>
+      <v-btn class="red mt-3" dark large :to="link">{{ btnText }}</v-btn>
     </v-layout>
   </v-parallax>
 </template>
--- src/components/organisms/TheToolber.vue
+++ src/components/organisms/TheToolber.vue
@@ -1,10 +1,17 @@
 <template>
   <v-toolbar color="red" dark>
-    <v-toolbar-title to="/">My Portfolio</v-toolbar-title>
+    <v-toolbar-title>
+      <v-avatar size="30px">
+        <v-img
+          :src="require('~/assets/logo/my-portfolio.png')"
+          alt="avatar"
+        ></v-img>
+      </v-avatar>
+      <span>My Portfolio</span>
+    </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
       <v-btn flat to="/">Home</v-btn>
-
       <v-btn flat to="/carrer">Carrer</v-btn>
     </v-toolbar-items>
   </v-toolbar>
